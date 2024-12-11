7373f59b7b06bf79328a357f75587bbb22708fa6
minhbro122 <Lynhan.zip.zip.zip@gmail.com>
2024-12-11 14:17:43 +0700

    Add files via upload

---
+++ Ss13_bài2.cpp
@@ -0,0 +1,31 @@
+#include <stdio.h>
+
+
+int ucln(int a, int b) {
+    while (b != 0) {
+        int temp = b;
+        b = a % b;
+        a = temp;
+    }
+    return a;
+}
+
+int main() {
+    int so1, so2;
+
+
+    printf("Nhap so thu nhat: ");
+    scanf("%d", &so1);
+
+    printf("Nhap so thu hai: ");
+    scanf("%d", &so2);
+
+
+    int ketQua = ucln(so1, so2);
+
+
+    printf("Uoc chung lon nhat cua %d va %d la: %d\n", so1, so2, ketQua);
+
+    return 0;
+}
+
